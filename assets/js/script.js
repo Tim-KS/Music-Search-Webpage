649d4e03814c7013e1bcc4192844066db3c42467
SanketPanchal <sanketp@altis.com.au>
2022-09-13 21:32:30 +1000

    Wiki pages show up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,15 @@
 var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#userinput');
-var wikiurl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search='
-var wikipage = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&titles=Pet_door&formatversion=2&rvprop=content&rvslots=*'
-var wikiHead = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&titles='
-var wikiTail = '&formatversion=2&rvprop=content&rvslots=*'
+
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    console.log('inside the form submit handler');
     var searchItem = nameInputEl.value.trim();
     if (searchItem) {
-        console.log(searchItem);
-        // searchItem = searchItem.split(' ').join('_')
-        console.log('new SearchItem is: ' + searchItem);
-        //getWiki(searchItem);
+        searchItem = searchItem.split(' ').join('_')
         tryWiki(searchItem);
     } else {
         alert('nice try buddy');
     }
-}
-
-var getWiki = function (artist) {
-    var apiURL = wikiurl + artist;
-    var apiPage = wikiHead + artist + wikiTail
-    //console.log(apiURL);
-    console.log(apiPage)
-    fetch(apiPage)
-        .then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    var name = data.query.pages[0].title
-                    console.log(data.query.pages[0].title);
-                    console.log(data.query.pages[0]);
-                    if (name) {
-                        document.getElementById("artist-name").textContent = name
-                    }
-                    var bio = data.query.pages[0].revisions[0].slots.main.content
-                    if (bio) {
-                        document.getElementById("bio").innerHTML = marked.parse(bio);
-                    }
-                    console.log(data.query.pages[0].revisions[0].slots.main.content);
-                    console.log('The data shown is above')
-                })
-            }
-        })
 }
 
 var tryWiki = function (artist) {
@@ -62,25 +29,15 @@ var tryWiki = function (artist) {
                     response.json().then(function (data) {
                         var json = data
                         console.log(json);
-                        var element = document.querySelector("wikipedia");
-                        element.append(json.parse.text["*"])
-                        console.log(element);
-                        console.log(json.parse.text["*"]);
-
+                        var content = json.parse.text["*"]
+                        var element = document.querySelector(".wikipedia");
+                        element.innerHTML = content
 
                     })
                 }
             }
             )
 
-        /*    
-          var req = fetch(url);
-          console.log(req);
-          var json = req.json();
-          console.log(json);
-          console.log(json.parse.text["*"]);
-  
-          */
     } catch (e) {
         console.error(e);
     }
