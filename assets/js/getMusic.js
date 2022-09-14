91530b9e3a7dadb254b4fe9c94e458d1fb849494
TimKS <tim.kandan-smith@outlook.com>
2022-09-14 20:29:52 +1000

    fixed error

--- assets/js/getMusic.js
+++ assets/js/getMusic.js
@@ -1,8 +1,6 @@
 var artistSearch = document.getElementById('artistSearch');
 var submitBtn = document.getElementById('submitBtn');
-
 var searchResult = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=&type=channel&key=AIzaSyDofZ01q_Qh-baUnjYC0t5fzULoMJw_qNE';
-
 var searchList = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=&type=channel&key=AIzaSyDofZ01q_Qh-baUnjYC0t5fzULoMJw_qNE';
 
 submitBtn.addEventListener('click', function (event) {
@@ -22,7 +20,6 @@ function getArtist(searchResult) {
             }
         })
     .then(function(data) {
-        console.log(data);
         var channelId = data.items[0].id.channelId;
         fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyDofZ01q_Qh-baUnjYC0t5fzULoMJw_qNE&channelId=' + channelId + '&part=snippet,id&type=video&chart=mostPopular&categoryId=10&maxResults=5')
         .then(function (response) {
@@ -30,7 +27,6 @@ function getArtist(searchResult) {
         })
         .then(function (data){
             console.log('YouTube Channel Top 5 Videos: \n----------');
-            console.log(data)
             for(var i = 0; i < data.items.length; i++) {
                 console.log('This is the index ' + i +' most popular video: ' + data.items[i].id.videoId);
             }
